4b6dbd686c02889dc873d210a1b544479c16fc94
damian.deleon <damianmdeleon@gmail.com>
2021-05-21 19:04:16 -0500

    updated router to Hashrouter for GitHub deployment compatability

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+import { HashRouter as Router, Route, } from "react-router-dom";
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
